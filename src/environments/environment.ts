46b59ca802791c6dd3e750f14179c9d956c73b7f
CGraves <cgraves@sl.on.ca>
2023-05-12 12:17:55 -0400

    with https aws (nconfirmed)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    serverUrl: 'http://msscpaspring-env.eba-h2sndt6n.us-east-2.elasticbeanstalk.com'
+    serverUrl: 'https://msscpaspring-env.eba-h2sndt6n.us-east-2.elasticbeanstalk.com'
   };
